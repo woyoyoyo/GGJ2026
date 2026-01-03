63c46fb9711244763979a65eb53aa474e7bdec07
Charles Vionnet <ch.vionnet@gmail.com>
2026-01-03 16:08:25 +0800

    ♻️ Added @export (access from Godot UI)

--- ggj-2026/scenes/main.gd
+++ ggj-2026/scenes/main.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-const GAME_DURATION = 10.0
-var time_elapsed: float = 0.0
+@export var game_duration: float = 10.0
 
-@onready var hud = $HUD
+var _time_elapsed: float = 0.0
+var _last_score: int = 0
+
+@onready var hud: HUD = $HUD
 
 
 func _ready() -> void:
@@ -14,14 +16,17 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if GameManager.is_game_running:
-		time_elapsed += delta
-		
+		_time_elapsed += delta
+
 		# Mettre à jour le HUD
-		hud.update_timer(time_elapsed)
-		
-		# Augmenter le score au fil du temps
-		GameManager.score = int(time_elapsed * 100)
-		
-		# Game over après 10 secondes
-		if time_elapsed >= GAME_DURATION:
+		hud.update_timer(_time_elapsed)
+
+		# Augmenter le score au fil du temps (seulement si changé)
+		var new_score: int = int(_time_elapsed * 100)
+		if new_score != _last_score:
+			_last_score = new_score
+			GameManager.score = new_score
+
+		# Game over après la durée configurée
+		if _time_elapsed >= game_duration:
 			GameManager.end_game()
