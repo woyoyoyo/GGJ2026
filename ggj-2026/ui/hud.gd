348f9985fc16cb9382a02cc5354c9de0f2177dc6
Charles Vionnet <ch.vionnet@gmail.com>
2026-01-03 16:06:19 +0800

    ♻️ Added variables types
    Without explicit type, using 'var' in GDScript creates a 'variant' (dynamic type), much slower

--- ggj-2026/ui/hud.gd
+++ ggj-2026/ui/hud.gd
@@ -1,7 +1,11 @@
 extends CanvasLayer
 
+class_name HUD
+
 @onready var score_label: Label = $MarginContainer/ScoreLabel
 @onready var timer_label: Label = $MarginContainer/TimerLabel
+
+var _last_time_displayed: int = -1
 
 
 func _ready() -> void:
@@ -10,9 +14,19 @@ func _ready() -> void:
 
 func _on_score_changed(new_score: int) -> void:
 	score_label.text = "Score: %d" % new_score
+	# Mettre à jour le timer basé sur le score (score = temps * 100)
+	var current_time: int = new_score / 100
+	if current_time != _last_time_displayed:
+		_last_time_displayed = current_time
+		var minutes: int = current_time / 60
+		var seconds: int = current_time % 60
+		timer_label.text = "Time: %d:%02d" % [minutes, seconds]
 
 
 func update_timer(time: float) -> void:
-	var minutes = int(time) / 60.0
-	var seconds = int(time) % 60
-	timer_label.text = "Time: %d:%02d" % [minutes, seconds]
+	var time_int: int = int(time)
+	if time_int != _last_time_displayed:
+		_last_time_displayed = time_int
+		var minutes: int = time_int / 60
+		var seconds: int = time_int % 60
+		timer_label.text = "Time: %d:%02d" % [minutes, seconds]
